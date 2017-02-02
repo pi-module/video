eaafb6459e31c45e7b13993fa18e00ccc652cbd3
voltan <djvoltan@gmail.com>
2017-02-02 15:23:43 +0330

    Update search system by new changes on pi

--- src/Api/Search.php
+++ src/Api/Search.php
@@ -58,7 +58,7 @@ class Search extends AbstractSearch
     /**
      * {@inheritDoc}
      */
-    protected function buildUrl(array $item, $table)
+    protected function buildUrl(array $item, $table = '')
     {
         switch ($table) {
             case 'category':
@@ -84,7 +84,7 @@ class Search extends AbstractSearch
     /**
      * {@inheritDoc}
      */
-    protected function buildImage(array $item)
+    protected function buildImage(array $item, $table = '')
     {
         // Get config
         $config = Pi::service('registry')->config->read($this->getModule());
