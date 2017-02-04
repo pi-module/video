716be7aa0a1249ce1a2cbbe2c59f7c50e8a37245
voltan <djvoltan@gmail.com>
2017-02-04 11:17:36 +0330

    Update search order

--- src/Api/Search.php
+++ src/Api/Search.php
@@ -59,7 +59,6 @@ class Search extends AbstractSearch
      * {@inheritDoc}
      */
     protected $order = array(
-        'recommended DESC',
         'time_create DESC',
         'id DESC'
     );
