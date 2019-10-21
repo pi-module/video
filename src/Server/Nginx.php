bc758bc453b1921eb9d4ca29cff1e2edb57664e6
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2019-10-21 14:06:10 +0330

    Finish getUrl method for nginx server

--- src/Server/Nginx.php
+++ src/Server/Nginx.php
@@ -15,6 +15,8 @@ namespace Module\Video\Server;
 
 class Nginx extends AbstractAdapter
 {
+    private $streamType = 'hls';
+
     public function updateMethod()
     {
         return [
@@ -33,6 +35,30 @@ class Nginx extends AbstractAdapter
 
     public function getUrl($params)
     {
-        return 'Nginx';
+        // Set stream type
+        $streamType = isset($params['streamType']) ? $params['streamType'] : $this->streamType;
+
+        // Set url
+        $url = $params['serverUrl'];
+        if (isset($params['serverPath']) && !empty($params['serverPath'])) {
+            $url = $url . '/' . $params['serverPath'];
+        }
+        if (isset($params['videoPath']) && !empty($params['videoPath'])) {
+            $url = $url . '/' . $params['videoPath'];
+        }
+
+        // Set video name end of url
+        switch ($streamType) {
+            default:
+            case 'hls':
+                $url = $url . '/' . $params['videoName'] . '/master.m3u8';
+                break;
+
+            case 'dash':
+                $url = $url . '/' . $params['videoName'] . '/manifest.mpd';
+                break;
+        }
+
+        return $url;
     }
 }
