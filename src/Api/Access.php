db324c16935c415ba2683f2239ed8bee1bd022e9
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2021-03-17 19:31:08 +0330

    work on access control

---
+++ src/Api/Access.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Pi Engine (http://piengine.org)
+ *
+ * @link            http://code.piengine.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://piengine.org
+ * @license         http://piengine.org/license.txt BSD 3-Clause License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+
+namespace Module\Video\Api;
+
+use Pi;
+use Pi\Application\Api\AbstractApi;
+
+/*
+ * Pi::api('access', 'video')->videoList($params);
+ */
+
+class Access extends AbstractApi
+{
+    public function generateKey($params)
+    {
+
+    }
+
+    public function validateKey($params)
+    {
+
+    }
+}
---
+++ src/Controller/Api/AccessController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Pi Engine (http://piengine.org)
+ *
+ * @link            http://code.piengine.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://piengine.org
+ * @license         http://piengine.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+
+namespace Module\Video\Controller\Api;
+
+use Pi;
+use Pi\Mvc\Controller\ApiController;
+use Laminas\Db\Sql\Predicate\Expression;
+
+class AccessController extends ApiController
+{
+    public function checkAction()
+    {
+
+
+
+
+
+
+
+
+
+    }
+}
